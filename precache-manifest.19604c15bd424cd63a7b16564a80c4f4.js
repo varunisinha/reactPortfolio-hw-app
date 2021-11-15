ccc98bf15606a58aa23df327c1d7fe6551f1ae23
varunisinha <varuni86@gmail.com>
2021-11-14 19:11:25 -0500

    Updates

---
+++ precache-manifest.19604c15bd424cd63a7b16564a80c4f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18bf00589e6d5280ee23facc0990736",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5cc9478c6a3bef8766cf",
+    "url": "./static/css/main.88ec6981.chunk.css"
+  },
+  {
+    "revision": "affa2342d7839c6629bb",
+    "url": "./static/js/2.f888a90c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.f888a90c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cc9478c6a3bef8766cf",
+    "url": "./static/js/main.4204d6ee.chunk.js"
+  },
+  {
+    "revision": "83075419c04aeaaf02cb",
+    "url": "./static/js/runtime-main.bb42c72b.js"
+  }
+]);
