4dbff17fb73367162e29ec2c51663961c23f9c1f
varunisinha <varuni86@gmail.com>
2021-11-14 21:04:56 -0500

    Updates

---
+++ precache-manifest.5ec89183701f68796e20ec5050b4f487.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578dadd1136015ff5b68fc8bc395a4ea",
+    "url": "/reactPortfolio-hw-app/index.html"
+  },
+  {
+    "revision": "2a17dd4f946de1d363ac",
+    "url": "/reactPortfolio-hw-app/static/css/main.88ec6981.chunk.css"
+  },
+  {
+    "revision": "affa2342d7839c6629bb",
+    "url": "/reactPortfolio-hw-app/static/js/2.f888a90c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactPortfolio-hw-app/static/js/2.f888a90c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a17dd4f946de1d363ac",
+    "url": "/reactPortfolio-hw-app/static/js/main.b0f35ebe.chunk.js"
+  },
+  {
+    "revision": "33928e6cb24ab3faadeb",
+    "url": "/reactPortfolio-hw-app/static/js/runtime-main.24029df8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b4405b92734de99db6e3111f93c40cb6.js"
+  "/reactPortfolio-hw-app/precache-manifest.5ec89183701f68796e20ec5050b4f487.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactPortfolio-hw-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
